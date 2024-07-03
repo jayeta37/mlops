efd14d9148b8a93c934d395c1d02d5c43a052812
Jayesh Tawade <jay37tawade@gmail.com>
2024-07-03 10:48:03 +0200

    Training pipeline completed and Requirements updated

--- package-ml-model/prediction_model/predict.py
+++ package-ml-model/prediction_model/predict.py
@@ -3,15 +3,17 @@ import pandas as pd
 import numpy as np
 import joblib
 from prediction_model.config import config
-from prediction_model.processing.data_handling import load_pipeline
+from prediction_model.processing.data_handling import load_dataset, load_pipeline
 
-classification_pipeline = load_pipeline(config.MODEL_NAME)
+classification_pipeline = load_pipeline()
 
-def generate_predictions(data_input):
-    data = pd.DataFrame(data_input)
-    prediction = classification_pipeline.predict(data[config.FEATURES])
+def generate_predictions(data_input = None):
+    test_data = load_dataset(config.TEST_FILE)
+    # data = pd.DataFrame(data_input)
+    prediction = classification_pipeline.predict(test_data[config.FEATURES])
     output = np.where(prediction == 1, 'Y', 'N')
     result = {"Predictions" : output}
+    print(result)
     return result
 
 if __name__=="__main__":
--- package-ml-model/prediction_model/training_pipeline.py
+++ package-ml-model/prediction_model/training_pipeline.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 from prediction_model.config import config
-from prediction_model.processing.data_handling import load_dataset, save_pipeline, load_pipeline
+from prediction_model.processing.data_handling import load_dataset, save_pipeline
 import prediction_model.processing.preprocessing as pp
 import prediction_model.pipeline as pipeline
-
+ 
 def perform_training():
     train_data = load_dataset(config.TRAIN_FILE)
     train_y = train_data[config.TARGET].map({'N': 0, 'Y': 1})
